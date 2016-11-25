235b0a316dd6d9451b42800dc3606d1c7c783764
Djip <jesper@finn-madsen.dk>
2016-11-25 10:58:54 +0100

    Menu Activity

--- app/src/main/java/orienteering/orienteering/MenuActivity.java
+++ app/src/main/java/orienteering/orienteering/MenuActivity.java
@@ -2,6 +2,11 @@ package orienteering.orienteering;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+
+import com.google.android.gms.vision.text.Line;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        final LinearLayout create_game_layout = (LinearLayout)findViewById(R.id.create_game_layout);
+        final LinearLayout open_world_layout = (LinearLayout)findViewById(R.id.open_world_game_layout);
+        final LinearLayout load_game_layout = (LinearLayout)findViewById(R.id.load_game_layout);
+
+        RadioButton create_game = (RadioButton)findViewById(R.id.create_game_radio);
+        create_game.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                create_game_layout.setVisibility(View.VISIBLE);
+                open_world_layout.setVisibility(View.GONE);
+                load_game_layout.setVisibility(View.GONE);
+            }
+        });
     }
 }
