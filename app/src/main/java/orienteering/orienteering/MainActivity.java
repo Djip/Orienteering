cc8fafe6e0f67104eeea5b0a2a2f15af9e868b8a
Draxxil <tobias-henriksen@hotmail.com>
2016-11-25 10:45:06 +0100

    Change design
    Changed design of app

--- app/src/main/java/orienteering/orienteering/MainActivity.java
+++ app/src/main/java/orienteering/orienteering/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         start_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                Intent intent = new Intent(MainActivity.this, MenuActivity.class);
                 startActivity(intent);
             }
         });
